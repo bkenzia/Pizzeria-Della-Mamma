662dac9e75fd87730ce18647c5ae078b25ea7182
thomasheroguelle <totosh93270@gmail.com>
2023-02-21 11:14:55 +0100

    gestion des conflits apres merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AllProductsComponent } from './pages/all-products/all-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path : 'all-products', component : AllProductsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
