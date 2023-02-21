8ad8be9f567e4b51657cfde7bbc5746b272f85d7
thomasheroguelle <totosh93270@gmail.com>
2023-02-21 10:50:35 +0100

    creation product card et product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { AllProductsComponent } from './pages/all-products/all-products.component';
+import { ProductCardsComponent } from './components/product-cards/product-cards.component';
+
+import { CentimesToEurosPipe } from './components/product-cards/product-cards.component';
+  // pipes for currency FR
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    AllProductsComponent,
+    ProductCardsComponent,
+    CentimesToEurosPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  // pipes for currency FR
   bootstrap: [AppComponent]
 })
 export class AppModule { }
