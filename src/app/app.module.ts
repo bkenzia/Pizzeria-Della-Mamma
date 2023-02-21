fb458ec24e77f6a811be25d520f0836145737c21
JulieNonnon <julie.nonnon@gmail.com>
2023-02-21 10:33:26 +0100

    [FEATURE] création HTML CSS component filtre catégories + style.css
    création HTML CSS du filtre catégories
    quelques ajouts de styles comme les fonts Inter et Playfair + le bacgroundcolor du site ont été ajouté respectivement en  index.html et style.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FilterByTagComponent } from './components/filter-by-tag/filter-by-tag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilterByTagComponent
   ],
   imports: [
     BrowserModule,
