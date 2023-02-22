e7b23547a6c151c6bfbe597474d100c0067aafc7
JulieNonnon <julie.nonnon@gmail.com>
2023-02-22 23:52:25 +0100

    [FEATURE] création page panier
    création page pnier, ajout des méthodes, routage de la page et lien en header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AllProductsComponent } from './pages/all-products/all-products.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { BasketComponent } from './pages/basket/basket.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'all-products', component: AllProductsComponent },
+  { path: 'basket', component: BasketComponent },
 ];
 
 @NgModule({
---
+++ src/app/pages/basket/basket.component.ts
@@ -0,0 +1,38 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-basket',
+//   templateUrl: './basket.component.html',
+//   styleUrls: ['./basket.component.css']
+// })
+// export class BasketComponent {
+
+// }
+
+import { Component } from '@angular/core';
+import { BasketProduct, BasketService } from 'src/app/services/basket/basket.service';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrls: ['./basket.component.css']
+})
+export class BasketComponent {
+  basket: BasketProduct[] = []; 
+
+  constructor(public basketService: BasketService) {};
+
+  ngOnInit() {
+    this.getbasket();
+  }
+
+  getbasket() {
+    this.basket = this.basketService.getBasket();
+  }
+ removeProduct(index: number) {
+  this.basketService.removeProduct(index);
+  
+  this.getbasket();
+  
+ }
+}
