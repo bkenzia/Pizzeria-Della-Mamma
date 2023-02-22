38564b2d57911d9bbdb0572a7423e95d348def17
thomasheroguelle <totosh93270@gmail.com>
2023-02-22 13:01:27 +0100

    pull

--- src/app/components/filter-by-tag/filter-by-tag.component.ts
+++ src/app/components/filter-by-tag/filter-by-tag.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { PRODUCTS } from 'src/app/mocks/products';
+import { ProductListService } from 'src/app/services/product-list.service';
+
 
 @Component({
   selector: 'app-filter-by-tag',
   templateUrl: './filter-by-tag.component.html',
   styleUrls: ['./filter-by-tag.component.css']
 })
-export class FilterByTagComponent {
+export class FilterByTagComponent implements OnInit {
+
+  constructor(private productsService: ProductListService) { }
+
+ ngOnInit() : void {
+  this.productsService.getProduct();
+  this.productsService.getProductsByTag(findTag);
+
+ }
 
 }
