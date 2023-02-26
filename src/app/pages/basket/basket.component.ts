fbf267c1de1ebfbb286d8bf49e301ec3478cf2e0
bkenzia <bkenzia@yahoo.fr>
2023-02-26 20:20:59 +0100

    la finalisation de l'affichage des produits dans la page panier

--- src/app/pages/basket/basket.component.ts
+++ src/app/pages/basket/basket.component.ts
@@ -1,31 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { BasketProduct, BasketService } from 'src/app/services/basket.service';
+
+@Pipe({ name: 'centimesToEuros' })
+export class CentimesToEurosPipe implements PipeTransform {
+  // transform = prend en parametre une valeur d'entree ('value') et retourne une valeur de sortie
+  transform(value: number): string {
+    //  vérifie si la valeur en entrée est nulle ou indéfinie. si oui, retourne une chaine vide
+    if (value == null) return '';
+
+    // si valeur valide, la méthode divise la valeur en entrée par 100 pour obtenir la valeur en euros
+    const euros = value / 100;
+
+    //  formate la valeur en euros pour avoir deux décimales après la virgule et on la concatène avec le symbole €
+    const eurosFormatted = euros.toFixed(2) + ' €';
+
+    // On retourne la valeur en euros
+    return eurosFormatted;
+  }
+}
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.css']
+  styleUrls: ['./basket.component.css'],
 })
 export class BasketComponent {
- basket: BasketProduct[] = []; 
+  basket: BasketProduct[] = [];
 
-constructor(
-  public basketService: BasketService
-  ) {};
+  constructor(public basketService: BasketService) {}
 
-ngOnInit() {
-   this.getbasket(); 
-   }
+  ngOnInit() {
+    this.getbasket();
+  }
 
- getbasket() {
+  getbasket() {
     this.basket = this.basketService.getBasket();
   }
 
-  removeProduct(index: number) {
-    this.basketService.removeProduct(index);
-    
-    this.getbasket();
-    
-   }
+  removeProduct() {
+    this.basketService.removeCart();
 
+    this.getbasket();
+  }
+  getNumber(): string | null {
+    const number = localStorage.getItem('number');
+    return number;
+  }
 }
--- src/app/pages/order-success/order-success.component.ts
+++ src/app/pages/order-success/order-success.component.ts
@@ -1,34 +1,18 @@
-
 import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-success',
   templateUrl: './order-success.component.html',
-  styleUrls: ['./order-success.component.css']
+  styleUrls: ['./order-success.component.css'],
 })
 export class OrderSuccessComponent {
-
-
   // constructor(private router: Router) {}
   // ngOnInit() {
 
   // }
-  // getNumber(): number | string {
-  //   const number = localStorage.getItem('number');
-
-  //   if (number != null) {
-  //     const numberTable = parseInt(number);
-  //     if (numberTable <= 14 && numberTable >= 1) {
-  //       return numberTable;
-  //     } else {
-  //       return '';
-  //     }
-  //   }
-  //   return '';
-  // }
-  // updateNamberTable() {
-  //   console.log(this.getNumber());
-  //   this.router.navigate(['']);
-  // }
+  getNumber(): string | null {
+    const number = localStorage.getItem('number');
+    return number;
+  }
 }
